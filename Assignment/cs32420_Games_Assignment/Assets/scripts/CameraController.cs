94fcb568cdb919e5466bdd1385263d860c1b2bb0
LauraMarie <lauramarie8131@gmail.com>
2020-11-10 12:40:23 +0100

    Camera follows

---
+++ Assignment/cs32420_Games_Assignment/Assets/scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10); 
+    }
+}
