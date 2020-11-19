5a4dde219fb784abf8a54d93c02d0866affce6d9
Laura Marie Wilkinson <lauramarie8131@gmail.com>
2020-11-19 14:16:48 +0100

    Rock sprite, falls and trigger gameover (#33)

---
+++ Assignment/cs32420_Games_Assignment/Assets/scripts/Rock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public bool rockTriggered;
+    public int rockSpeed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rockTriggered = false;
+        rockSpeed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Rock OnTriggerEnter triggered");
+            rockTriggered = true; 
+        }
+        
+    }
+
+    public void rockMovement()
+    {
+        Debug.Log("Rock rockMovement");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-rockSpeed, GetComponent<Rigidbody2D>().velocity.x);
+    }
+}
